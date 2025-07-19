576d43763339162c619e4c49d202504623a21763
PANDURANGZURE <pandurangzure6@gmail.com>
2025-07-19 20:42:39 +0530

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import logo from "../assets/logo.png"
 
 function Header() {
   return (
     <>
      <div className="flex justify-center items-center flex-col sticky top-0 z-50  " >
-            <p className="absolute text-6xl logo top-10  ">saurav</p>
+            <div className='absolute -top-1 rotate-3 mr-1 '>
+              <img className='h-32' src={logo} alt="" />
+            </div>
             <div className="bg-[#F4EADE] w-full md:h-20 h-16 shadow-2xl "></div>
             <div className="w-0 h-0 border-l-[150px] border-l-transparent 
-            border-t-[70px] border-t-[#F4EADE]
+            border-t-[60px] border-t-[#F4EADE]
             border-r-[150px] border-r-transparent
             "></div>
             </div>
